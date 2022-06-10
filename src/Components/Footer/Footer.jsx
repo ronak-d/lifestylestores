03c73921790a42decb892547ef0926c7f26ae671
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-06-11 03:01:14 +0530

    Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import styles from "./footer.module.css"
+
+const Footer = () =>{
+  return (
+    <div className={styles.f_whole}>
+    <div className={styles.f_main}>
+        <div className={styles.f_div}>
+            <h3 className={styles.f_h}>Women</h3>
+            <h5 className={styles.f_sub_head} >Tops</h5>
+            <h5 className={styles.f_sub_head} >Ethnicwear</h5>
+            <h5 className={styles.f_sub_head} >Bottoms</h5>
+            <h5 className={styles.f_sub_head} >Dresses & Jumpsuits</h5>
+            <h5 className={styles.f_sub_head} >Winterwear</h5>
+            <h5 className={styles.f_sub_head} >Lingerie</h5>
+            <h5 className={styles.f_sub_head} >Nightwear</h5>
+            <h5 className={styles.f_sub_head} >Sportswear</h5>
+            <h5 className={styles.f_sub_head} >Beauty</h5>
+            <h5 className={styles.f_sub_head} >Watches & Sunglasses</h5>
+        </div>
+
+        <div className={styles.f_div}>
+            <h3 className={styles.f_h}>Men</h3>
+            <h5 className={styles.f_sub_head} >Tops</h5>
+            <h5 className={styles.f_sub_head} >Bottoms</h5>
+            <h5 className={styles.f_sub_head} >Ethnicwear</h5>
+            <h5 className={styles.f_sub_head} >Winterwear</h5>
+            <h5 className={styles.f_sub_head} >Sportswear</h5>
+            <h5 className={styles.f_sub_head} >Innerwear</h5>
+            <h5 className={styles.f_sub_head} >Grooming</h5>
+            <h5 className={styles.f_sub_head} >Sportswear</h5>
+            <h5 className={styles.f_sub_head} >Watches</h5>
+            <h5 className={styles.f_sub_head} >Sunglasses</h5>
+        </div>
+
+        <div className={styles.f_div}>
+            <h3 className={styles.f_h}>Kids</h3>
+            <h5 className={styles.f_sub_head} >Girls Clothing</h5>
+            <h5 className={styles.f_sub_head} >Boys Clothing</h5>
+            <h5 className={styles.f_sub_head} >Infants Girls</h5>
+            <h5 className={styles.f_sub_head} >Infants Boys</h5>
+            <h5 className={styles.f_sub_head} >Winterwear</h5>
+            <h5 className={styles.f_sub_head} >Add ons</h5>
+            <h5 className={styles.f_sub_head} >The Teen Shop</h5>
+        </div>
+
+        <div className={styles.f_div}>
+            <h3 className={styles.f_h}>Shoes & Bags</h3>
+            <h5 className={styles.f_sub_head} >Women</h5>
+            <h5 className={styles.f_sub_head} >Men</h5>
+            <h5 className={styles.f_sub_head} >Boys</h5>
+            <h5 className={styles.f_sub_head} >Girls</h5>
+            <h5 className={styles.f_sub_head} >Women Accessories</h5>
+            <h5 className={styles.f_sub_head} >Men Accessories</h5>
+            <h5 className={styles.f_sub_head} >Essential</h5>
+        </div>
+
+        <div className={styles.f_div}>
+            <h3 className={styles.f_h}>Explore</h3>
+            <h5 className={styles.f_sub_head} >Online Offers</h5>
+            <h5 className={styles.f_sub_head} >Store Offers</h5>
+            <h5 className={styles.f_sub_head} >Online Gift Card</h5>
+            <h5 className={styles.f_sub_head} >Store Gift Card</h5>
+            <h5 className={styles.f_sub_head} >Stores Nearby</h5>
+            <h5 className={styles.f_sub_head} >EDGE Membership</h5>
+            <h5 className={styles.f_sub_head} >Refer & Win</h5>
+            <h5 className={styles.f_sub_head} >Shop on WhatsApp</h5>
+            <h5 className={styles.f_sub_head} >Fashion VLOG</h5>
+            <h5 className={styles.f_sub_head} >Join CIRCLEMAG</h5>
+            <h5 className={styles.f_sub_head} >SBI Offers</h5>
+            <h5 className={styles.f_sub_head} >Homecentre</h5>
+        </div>
+    </div>
+
+    
+    <div className={styles.f_main2}>
+        <div className={styles.f_div_2}>
+            <h3 className={styles.f_h}>Explore</h3>
+            <h5 className={styles.f_sub_head} >Online</h5>
+            <h5 className={styles.f_sub_head} >Offers</h5>
+            <h5 className={styles.f_sub_head} >Store</h5>
+            <h5 className={styles.f_sub_head} >Online</h5>
+            <h5 className={styles.f_sub_head} >Gift Card</h5>
+            <h5 className={styles.f_sub_head} >Membership</h5>
+        </div>
+
+        <div className={styles.f_div_2}>
+            <h3 className={styles.f_h}>About</h3>
+            <h5 className={styles.f_sub_head} >About us</h5>
+            <h5 className={styles.f_sub_head} >Careers</h5>
+            <h5 className={styles.f_sub_head} >Take a Tour</h5>
+            <h5 className={styles.f_sub_head} >Blog</h5>
+            <h5 className={styles.f_sub_head} >Store Locator</h5>
+            <h5 className={styles.f_sub_head} >Landmark Cares</h5>
+        </div>
+
+        <div className={styles.f_div_2}>
+            <h3 className={styles.f_h}>Help</h3>
+            <h5 className={styles.f_sub_head} >Contact us</h5>
+            <h5 className={styles.f_sub_head} >Shipping</h5>
+            <h5 className={styles.f_sub_head} >Returns Process</h5>
+            <h5 className={styles.f_sub_head} >Returns Policy</h5>
+            <h5 className={styles.f_sub_head} >Help Centre</h5>
+        </div>
+    </div>
+    </div>
+  )
+}
+export default Footer;
